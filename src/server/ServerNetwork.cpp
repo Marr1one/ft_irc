3a4b55115ba10f8cc7132a58f6cb02ae150d6826
El Houssain Souhail <sou.ductive@gmail.com>
2026-03-17 16:32:19 +0100

    fix: indentation error

--- src/server/ServerNetwork.cpp
+++ src/server/ServerNetwork.cpp
@@ -6,12 +6,12 @@
 /*   By: esouhail <esouhail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 22:29:05 by esouhail          #+#    #+#             */
-/*   Updated: 2026/03/17 14:43:09 by esouhail         ###   ########.fr       */
+/*   Updated: 2026/03/17 16:30:23 by esouhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Server.hpp"
 #include "Parser.hpp"
+#include "Server.hpp"
 
 void Server::acceptClient() {
 	int client_fd = accept(_serverFd, NULL, NULL);
@@ -50,7 +50,7 @@ void Server::receiveMessage(int i) {
 		if (!Parser::parse(line, msg))
 			continue;
 
-			handleMessage(fd, msg);
+		handleMessage(fd, msg);
 	}
 }
 
@@ -75,30 +75,27 @@ void Server::removeClientFromPoll(int fd) {
 	}
 }
 
-void Server::sendChannelMsg(int fd, const std::string &channelName, const std::string &msg)
-{
-    if (_channels.find(channelName) == _channels.end())
-        std::cout << "Channel name not found!\n";
-    else
-        _channels[channelName].broadcast(fd, msg);
+void Server::sendChannelMsg(int fd, const std::string &channelName,
+							const std::string &msg) {
+	if (_channels.find(channelName) == _channels.end())
+		std::cout << "Channel name not found!\n";
+	else
+		_channels[channelName].broadcast(fd, msg);
 }
 
-void Server::sendUserMsg(int fd, const std::string &target, const std::string &msg)
-{
-    (void)fd;
-    for(ClientIt it = _clients.begin(); it != _clients.end(); it++)
-    {
-        if (it->second.get_nickname()==target)
-        {
-            send(it->second.get_fd(), msg.c_str(), msg.size(), 0);
-            return ;
-        }
-    }
-    std::cout << "User not found!\n";
+void Server::sendUserMsg(int fd, const std::string &target,
+						 const std::string &msg) {
+	(void)fd;
+	for (ClientIt it = _clients.begin(); it != _clients.end(); it++) {
+		if (it->second.get_nickname() == target) {
+			send(it->second.get_fd(), msg.c_str(), msg.size(), 0);
+			return;
+		}
+	}
+	std::cout << "User not found!\n";
 }
 
-void Server::sendReply(int fd, const std::string &reply)
-{
-    std::string msg = reply + "\r\n";
-    send(fd, msg.c_str(), msg.size(), 0);
+void Server::sendReply(int fd, const std::string &reply) {
+	std::string msg = reply + "\r\n";
+	send(fd, msg.c_str(), msg.size(), 0);
 }
